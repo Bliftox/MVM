7d239dc863abfb65ae6b59289f5d5bece04cff4f
NoDarckName <130218532+NoDarckName@users.noreply.github.com>
2023-12-26 22:08:06 +0300

    first commit

---
+++ src/main/java/org/harbingers_of_chaos/mvm/MystiVerseModServer.java
@@ -0,0 +1,13 @@
+package org.harbingers_of_chaos.mvm;
+
+import net.fabricmc.api.ModInitializer;
+
+public class MystiVerseModServer implements ModInitializer {
+    /**
+     * Runs the mod initializer.
+     */
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/org/harbingers_of_chaos/mvm/client/MystiVerseModServerClient.java
@@ -0,0 +1,13 @@
+package org.harbingers_of_chaos.mvm.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MystiVerseModServerClient implements ClientModInitializer {
+    /**
+     * Runs the mod initializer on the client environment.
+     */
+    @Override
+    public void onInitializeClient() {
+
+    }
+}

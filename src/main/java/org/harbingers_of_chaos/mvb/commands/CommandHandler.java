34645b5c221c65494450bec6492718ab4bf9d238
LingExe <151561736+LingExe@users.noreply.github.com>
2023-12-30 14:13:32 +0100

    1.2.1 Commands update

--- src/main/java/org/harbingers_of_chaos/mvb/commands/CommandHandler.java
+++ src/main/java/org/harbingers_of_chaos/mvb/commands/CommandHandler.java
@@ -1,10 +1,12 @@
 package org.harbingers_of_chaos.mvb.commands;
 
+import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.interactions.components.buttons.Button;
+import net.dv8tion.jda.api.entities.Role;
 
-import static org.harbingers_of_chaos.mvb.Main.log;
+import static org.harbingers_of_chaos.mvb.Main.*;
 
 public class CommandHandler extends ListenerAdapter {
 
@@ -13,13 +15,35 @@ public class CommandHandler extends ListenerAdapter {
         if (event.getAuthor().isBot()) {return;}
         String message = event.getMessage().getContentDisplay();
 
-        if (message.equalsIgnoreCase("$привет")) {
-            event.getMessage().reply("jepa").addActionRow(Button.primary("Hello", "Jepa")).queue();
-            event.getMessage().reply("jepa1").addActionRow(Button.primary("suggestButton", "suggestion")).queue();
-            log.info("Command");
+        // Получение объекта роли по её ID
+        Role role = event.getGuild().getRoleById("1185653602056933436");
 
+        // Проверка, не является ли роль null (не была ли роль найдена)
+        if (role != null) {
+            // Проверка наличия у роли конкретного права (например, ADMINISTRATOR)
+            boolean hasAdminPermission = role.hasPermission(Permission.ADMINISTRATOR);
+
+            // Вывод результата в консоль или другую обработку
+            if (hasAdminPermission) {
+                if (message.equalsIgnoreCase("&stop")) {
+                    event.getJDA().shutdown();
+                    event.getMessage().reply("Успешная остановка! 🎄").queue();
+
+                } else if (message.equalsIgnoreCase("$buttons")) {
+                    event.getMessage().reply("jepa").addActionRow(Button.primary("Hello", "Jepa")).queue();
+                    event.getMessage().reply("jepa1").addActionRow(Button.primary("suggestButton", "suggestion")).queue();
+                    log.info("Command");
+                } else if (message.equalsIgnoreCase("$help")) {
+                    event.getMessage().reply("```&stop``` - остановить бота." +
+                            "\n```$buttons``` - кнопки взаимодействия.").queue();
+
+                }
+
+            } else {
+                log.warning("Not enough rights");
+            }
+        } else {
+            log.warning("Role not found");
         }
     }
-
-
 }

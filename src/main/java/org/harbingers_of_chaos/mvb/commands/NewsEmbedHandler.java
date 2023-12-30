c1a569be842a36a748a538a75dc5569a56c4a910
LingExe <151561736+LingExe@users.noreply.github.com>
2023-12-30 17:18:38 +0100

    1.3 DataBase

---
+++ src/main/java/org/harbingers_of_chaos/mvb/commands/NewsEmbedHandler.java
@@ -0,0 +1,30 @@
+package org.harbingers_of_chaos.mvb.commands;
+
+import net.dv8tion.jda.api.events.guild.GuildReadyEvent;
+import net.dv8tion.jda.api.events.guild.update.GuildUpdateSplashEvent;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.interactions.commands.build.CommandData;
+import net.dv8tion.jda.api.interactions.commands.build.Commands;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.harbingers_of_chaos.mvb.Main.*;
+import static org.harbingers_of_chaos.mvb.commands.CommandHandler.*;
+public class NewsEmbedHandler extends ListenerAdapter {
+
+    @Override
+    public void onSlashCommandInteraction(SlashCommandInteractionEvent event) {
+        String command = event.getName();
+    }
+
+    @Override
+    public void onGuildReady(GuildReadyEvent event) {
+        List<CommandData> commandData = new ArrayList<>();
+        commandData.add(Commands.slash("News", "Create news embed easy"));
+        event.getGuild().updateCommands().addCommands(commandData).queue();
+
+    }
+}
